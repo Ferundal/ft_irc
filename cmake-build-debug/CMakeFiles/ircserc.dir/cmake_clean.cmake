0f3d8c0bda81013e6c3f40db2e9adefa6617bc2f
kreker008 <forwork@HOME-PC.localdomain>
2022-01-11 22:29:36 +0300

    server supports multiple connections

---
+++ cmake-build-debug/CMakeFiles/ircserc.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ircserc.dir/src/ClientSocket.cpp.o"
+  "CMakeFiles/ircserc.dir/src/ConnectSocket.cpp.o"
+  "CMakeFiles/ircserc.dir/src/Server.cpp.o"
+  "CMakeFiles/ircserc.dir/src/main.cpp.o"
+  "ircserc"
+  "ircserc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ircserc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
