88ffde883bf551b063babe5c9042ee2e9c874750
ferundal <ferundal@gmail.com>
2022-01-22 20:58:04 +0300

    no more vector, it is time for LIST

--- src/UserInfoStore.cpp
+++ src/UserInfoStore.cpp
@@ -16,8 +16,8 @@ User &UserInfoStore::CreateNewUser(int _new_user_fd) {
 }
 
 bool UserInfoStore::IsNickAvalable(string const _searching_nick) const {
-	vector<User>::const_iterator _curr_user = _users_store.begin();
-	vector<User>::const_iterator _end = _users_store.end();
+	list<User>::const_iterator _curr_user = _users_store.begin();
+	list<User>::const_iterator _end = _users_store.end();
 	while (_curr_user != _end) {
 		if (_curr_user->_nick == _searching_nick)
 			return (false);
@@ -27,10 +27,10 @@ bool UserInfoStore::IsNickAvalable(string const _searching_nick) const {
 }
 
 bool UserInfoStore::IsUserActive(string const _searching_nick) const {
-	vector<string>::const_iterator _curr_active_user_name = _connected_users.begin();
-	vector<string>::const_iterator _end = _connected_users.end();
+	list<User *>::const_iterator _curr_active_user_name = _connected_users.begin();
+	list<User *>::const_iterator _end = _connected_users.end();
 	while (_curr_active_user_name != _end) {
-		if (*_curr_active_user_name == _searching_nick)
+		if ((*_curr_active_user_name)->_nick == _searching_nick)
 			return (true);
 		++_curr_active_user_name;
 	}
@@ -38,25 +38,39 @@ bool UserInfoStore::IsUserActive(string const _searching_nick) const {
 }
 
 User *UserInfoStore::FindUserByNick(string _searching_nick) {
-	if (IsUserActive(_searching_nick) == true) {
-		vector<User>::iterator _curr_user = _users_store.begin();
-		vector<User>::iterator _end = _users_store.end();
-		while (_curr_user != _end) {
-			if (_curr_user->_nick == _searching_nick)
-				return (&*_curr_user);
-			++_curr_user;
-		}
+	list<User *>::iterator _curr_user = _connected_users.begin();
+	list<User *>::iterator _end = _connected_users.end();
+	while (_curr_user != _end) {
+		if ((*_curr_user)->_nick == _searching_nick)
+			return (*_curr_user);
+		++_curr_user;
 	}
 	return (NULL);
 }
 
 Channel *UserInfoStore::FindChannelByName(string _searching_channel_name) {
-	vector<Channel>::iterator _curr_channel = _active_channels.begin();
-	vector<Channel>::iterator _end = _active_channels.end();
+	list<Channel>::iterator _curr_channel = _active_channels.begin();
+	list<Channel>::iterator _end = _active_channels.end();
 	while (_curr_channel != _end) {
 		if(_curr_channel->_channel_name == _searching_channel_name)
 			return (&*_curr_channel);
 		++_curr_channel;
 	}
 	return (NULL);
+}
+
+void UserInfoStore::PrintUserInfoStore() {
+	list<User>::iterator _curr_user = _users_store.begin();
+	list<User>::iterator _end_user = _users_store.end();
+	while (_curr_user != _end_user) {
+		std::cout << _curr_user->_fd << " " << _curr_user->_nick << " " << _curr_user->_real_name << std::endl;
+		++_curr_user;
+	}
+	list<User *>::iterator _curr_nick = _connected_users.begin();
+	list<User *>::iterator _end_nick = _connected_users.end();
+	while (_curr_nick != _end_nick) {
+		std::cout << *_curr_nick << std::endl;
+		++_curr_nick;
+	}
+
 }
--- src/UserInfoStore.hpp
+++ src/UserInfoStore.hpp
@@ -4,11 +4,13 @@
 
 #ifndef FT_IRC_USERINFOSTORE_HPP
 #define FT_IRC_USERINFOSTORE_HPP
-#include <vector>
+#include <list>
+#include <iostream>
 #include "User.hpp"
 #include "Channel.hpp"
 
-using std::vector;
+
+using std::list;
 using std::string;
 
 class User;
@@ -18,20 +20,18 @@ class UserInfoStore {
 	friend class User;
 	friend class Channel;
 private:
-	vector<User> _users_store;
-	vector<string> _connected_users;
-	vector<Channel> _active_channels;
-private:
-
+	list<User> _users_store;
+	list<User *> _connected_users;
+	list<Channel> _active_channels;
 public:
 	UserInfoStore();
 	~UserInfoStore();
 	bool IsNickAvalable(const string _searching_nick) const;
 	bool IsUserActive(const string _searching_nick) const;
 	User &CreateNewUser(int _new_user_fd);
-
 	User *FindUserByNick(string _searching_nick);
 	Channel *FindChannelByName(string _searching_channel_name);
+	void PrintUserInfoStore();
 };
 
 
