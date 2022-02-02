96b30435c7b99e0abc99bb9253c38ce858568adb
ferundal <ferundal@gmail.com>
2022-02-02 17:34:50 +0300

    Pass is too long error added

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,20 @@ const char* GLOBAL_PASS;
 #include "Server.hpp"
 #include "iostream"
 
-int check_arg(int argc, char **argv) {
+int check_arg(int argc,const char **argv) {
 	(void)argv;
 	if (argc != 3) {
 		std::cout << "Wrong param amount. Expected two: port and password" <<std::endl;
 		return (1);
 	}
+	if(std::string().append(argv[2]).size() > 505) {
+		std::cout << "Password is too long!" <<std::endl;
+		return (1);
+	}
 	return (0);
 }
 
-int main(int argc, char **argv) {
+int main(int argc,const char **argv) {
 
 	(void)argc;
 	(void)argv;
